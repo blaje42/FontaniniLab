d46a9a994b66a09d89b286328cdf5ee64772ac15
blaje42 <blaje42@gmail.com>
2019-09-19 17:06:49 -0400

    Fixed error using readmatrix and added code to extract # of trials
    readmatrix dropping first row of lick ILI's for some reason and can't read a single cell so changed to xlsread

--- Behavior/importBATdata.m
+++ Behavior/importBATdata.m
@@ -1,4 +1,4 @@
-function [BATdata, LICKdata] = importBATdata(FileID,nTrialTot)
+function [BATdata, LICKdata,nTrialTot] = importBATdata(FileID)
 % Import data for Brief Access Test from Davis Rig (already converted from
 % .txt to .xlsx format
 %
@@ -9,12 +9,14 @@ function [BATdata, LICKdata] = importBATdata(FileID,nTrialTot)
 %
 % OUTPUTS:
 %   BATdata (table) = contains table of information for each trial
-%   LICKdata (array) = contains lick time onsets (ms relative to first
+%   LICKdata (array) = contains trial # (column 1) and lick time onsets (ms relative to first
 %                      lick) for each trial (row = trial)
 
+nTrialTot = xlsread([FileID '.xlsx'],'B10:B10');
 rangeID1 = ['A11:K' num2str(11+nTrialTot)]; %Excel range for 
 
 BATdata = readtable([FileID '.xlsx'], 'Range', rangeID1);
 
-rangeID2 = ['B' num2str(nTrialTot+13) ':ZZ' num2str(nTrialTot+87)];
-LICKdata = readmatrix([FileID '.xlsx'],'Range',rangeID2);
+rangeID2 = ['A' num2str(nTrialTot+13) ':ZZ' num2str(2*nTrialTot+12)];
+LICKdata = xlsread([FileID '.xlsx'],rangeID2);
+%LICKdata = readmatrix([FileID '.xlsx'],'Range',rangeID2); %This is skipping the first row for some reason...so if you need to use it find a fix for it
