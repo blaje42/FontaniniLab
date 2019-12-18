999b0a60ee480ecac298a4f2847a09da922881a5
blaje42 <blaje42@gmail.com>
2019-12-18 16:22:53 -0500

    Add plot for consumption over days

--- Behavior/CTAanalysis_OneBottle.m
+++ Behavior/CTAanalysis_OneBottle.m
@@ -2,13 +2,14 @@
 %apparatus 
 %(MUST RUN THIS SECTION BEFORE ANY OF THE OTHER SECTIONS EVERY TIME)
 
-MouseID = 'TDPWM_006';
+MouseID = 'TDPQM_022';
 age = '12wk';
 
 rootdir = 'C:\Users\Jennifer\Documents\DATA\BEHAVIOR';
 sep = '\';
 CTAtest = 'Sucrose'; %Tastant used for CTA test
 testfolderID = 'PreferenceTest';
+nContextHab = 2; %Number of habituation trials
 OutputFileName = [MouseID '-CTA-' CTAtest '-' age];
 
 % Pre-sets for figures
@@ -20,8 +21,12 @@ set(groot,{'DefaultAxesXColor','DefaultAxesYColor','DefaultAxesZColor'},{'k','k'
 %  *****                       IMPORT DATA                     *****
 %  *****************************************************************
 
+%%%%% Change import to reflect date -- change folder names to include date at beginning so order is by day %%%%%
+
 %Identify subfolders containing data on test days. 
 d = dir([rootdir sep MouseID sep age]);
+d(1:2,:) = [];
+d(1:nContextHab,:) = [];
 isub = [d(:).isdir]; % returns logical vector
 nameFolds = {d(isub).name}';
 folderIDs = {CTAtest, testfolderID, 'Water'}; %Identifying string for test folder
@@ -39,18 +44,29 @@ for fold = 1:length(folderIDs)
             error('Import error - not all trials imported, FIX before proceeding')
         end
 
-        ILIdataALL.(folderIDs{fold}){u} = ILIdata;
+        ILItableALL.(folderIDs{fold}){u} = ILIdata;
         CTAtableALL.(folderIDs{fold}){u} = CTAtable;
     end
 
 end
 
+clear CTAtable ILIdata
+for fold = 1:length(nameFolds)
+    cd([rootdir sep MouseID sep age sep nameFolds{fold}]);
+    foldID = nameFolds{fold}(isstrprop(nameFolds{fold},'alpha'));
+    [CTAtable{fold}, ILIdata{fold}, nTrialTot] = importDAVISdata([MouseID '_' foldID]); 
+    CTAtable{fold}.SOLUTION = foldID;
+    
+end
+
+
+
     
 cd([rootdir sep MouseID]);
-save([OutputFileName '.mat'],'CTAtableALL','ILItableALL'); fprintf('Saving... %s\n', OutputFileName);
+save([OutputFileName '.mat'],'CTAtableALL','ILItableALL','CTAtable','ILIdata'); fprintf('Saving... %s\n', OutputFileName);
 
 disp('*************************************');
-disp(['Imported ' num2str(length(testFolds)) ' test(s) for ' MouseID]);
+disp(['Imported ' num2str(length(d)) ' test(s) for ' MouseID]);
 disp('*************************************');
 
 %% ****************************************************************
@@ -74,9 +90,25 @@ end
 
 save(OutputFileName,'lickCount','-append'); fprintf('Appending... %s\n', OutputFileName);
 
-%%%%%%% I. Plot consumption of conditioned tastant over days %%%%%%%
+%%%%%%% I. Plot consumption over days %%%%%%%
 
-figure; subplot(1,2,1)
+figure; subplot(2,2,[1 2]); hold on;
+for u = 1:length(CTAtable)
+    if strcmp(CTAtable{u}.SOLUTION,CTAtest)
+        bar(u,CTAtable{u}.LICKS,0.4,'m','EdgeColor','none')
+    elseif strcmp(CTAtable{u}.SOLUTION,'Water')
+        bar(u,CTAtable{u}.LICKS,0.4,'b','EdgeColor','none')
+    else bar(u,CTAtable{u}.LICKS,0.4,'r','EdgeColor','none')
+    end
+    ID{u} = CTAtable{u}.SOLUTION;
+end
+
+set(gca,'TickDir','out','XTick',1:length(ID),'XTickLabel',ID)
+ylabel('Licks');
+
+%%%%%%% II. Plot consumption of conditioned tastant over days %%%%%%%
+
+subplot(2,2,3)
 bar(lickCount.(CTAtest),0.4,'EdgeColor','none');
 hold on; bar(length(lickCount.(CTAtest))+1,lickCount.(testfolderID),0.4,'EdgeColor','none');
 set(gca,'TickDir','out')
@@ -84,17 +116,17 @@ box off;
 ylabel('Licks'); xlabel('Sucrose presentation')
 
 
-%%%%%%% II. Plot consumption of conditioned tastant on test day vs. water %%%%%%%
+%%%%%%% III. Plot consumption of conditioned tastant on test day vs. water %%%%%%%
 
-subplot(1,2,2)
-bar([lickCount.Water lickCount.(testfolderID)],0.4,'EdgeColor','none');
+subplot(2,2,4)
+bar([lickCount.Water(end) lickCount.(testfolderID)],0.4,'EdgeColor','none');
 set(gca,'TickDir','out','XTickLabels',{'Water', CTAtest})
 box off;
 ylabel('Licks'); xlabel('Tastant')
 
 
 sgtitle(MouseID,'FontSize',20,'Color','red','Interpreter', 'none')
-ppsize = [1200 400];
+ppsize = [1600 1200];
 set(gcf,'PaperPositionMode','auto');         
 set(gcf,'PaperOrientation','landscape');
 set(gcf,'PaperUnits','points');
