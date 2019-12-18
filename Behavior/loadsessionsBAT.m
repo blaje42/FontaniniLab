fd021706cfceb8c1b7794488dd894fea7aa3ce98
blaje42 <blaje42@gmail.com>
2019-12-18 16:21:59 -0500

    Mouse IDs for subgroups

---
+++ Behavior/loadsessionsBAT.m
@@ -0,0 +1,35 @@
+function MouseIDs = loadsessionsBAT(cohortID,age)
+
+if strcmp(cohortID,'MUT')
+    
+  if strcmp(age,'12wk')
+      MouseIDs = {'TDPQM_002', 'TDPQM_008', 'TDPQF_005', 'TDPQF_010', 'TDPQF_011','TDPQF_015','TDPQF_016',...
+      'TDPQF_017'};        
+  end
+  
+  if strcmp(age,'20wk')
+      MouseIDs = {'TDPQF_005', 'TDPQM_010'};      
+  end
+    
+elseif strcmp(cohortID,'CTRL')
+        
+  if strcmp(age,'12wk')
+      MouseIDs = {'TDPWF_002','TDPWM_016','TDPWM_001','TDPWM_002','TDPWM_005','TDPWM_011','TDPWF_005','TDPWF_006','TDPWF_007','TDPWF_008',...
+      'TDPWF_011'};       
+  end
+  
+  if strcmp(age,'20wk')
+      MouseIDs = {'TDPWF_002','TDPWM_001','TDPWM_002','TDPWM_005','TDPWM_011'};         
+  end
+ 
+elseif strcmp(cohortID,'WT')
+        
+  if strcmp(age,'12wk')
+      MouseIDs = {'TDPQF_006', 'TDPQM_011', 'TDPWF_001','TDPQF_012','TDPQM_015','TDPQF_018','TDPQF_019'};     
+  end
+  
+  if strcmp(age,'20wk')
+      MouseIDs = {'TDPQF_001','TDPQF_006','TDPQM_011','TDPWF_001'};
+  end
+    
+end
